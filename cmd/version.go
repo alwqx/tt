bcf09163763cc32884f6020d198bd123df2d1c25
adolphlwq <kenan3015@gmail.com>
2017-02-27 11:27:07 +0800

    add version command

---
+++ cmd/version.go
@@ -0,0 +1,48 @@
+package cmd
+
+import (
+	"fmt"
+	"runtime"
+
+	"github.com/spf13/cobra"
+)
+
+const (
+	TT_VERSION = "0.1"
+)
+
+type VersionInfo struct {
+	TTVersion string
+	GoVersion string
+	OSArch    string
+}
+
+var versionCmd = &cobra.Command{
+	Use:   "version",
+	Short: "show version of tt",
+	Run:   versionRun,
+}
+
+func versionRun(cmd *cobra.Command, args []string) {
+	info := getTTVersion()
+	fmt.Println("tt version:", info.TTVersion)
+	fmt.Println("go version:", info.GoVersion)
+	fmt.Println("os/arch:   ", info.OSArch)
+}
+
+func init() {
+	RootCmd.AddCommand(versionCmd)
+}
+
+func getTTVersion() *VersionInfo {
+	var versionInfo = &VersionInfo{
+		TTVersion: TT_VERSION,
+		GoVersion: runtime.Version(),
+		OSArch:    getOSArch(),
+	}
+	return versionInfo
+}
+
+func getOSArch() string {
+	return runtime.GOOS + "/" + runtime.GOARCH
+}
